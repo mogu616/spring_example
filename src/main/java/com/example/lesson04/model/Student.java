1cde89c92713177e95f65a00ee16978a6879cf0a
DESKTOP-SNDKGQM\gimt6 <jtk6747@gmail.com>
2022-09-28 01:26:31 +0900

    220927 학원

---
+++ src/main/java/com/example/lesson04/model/Student.java
@@ -0,0 +1,70 @@
+package com.example.lesson04.model;
+
+import java.util.Date;
+
+public class Student {
+	private int id;
+	private String name;
+	private String phoneNumber;
+	private String email;
+	private String dreamJob;
+	private Date createdAt;
+	private Date updatedAt;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDreamJob() {
+		return dreamJob;
+	}
+
+	public void setDreamJob(String dreamJob) {
+		this.dreamJob = dreamJob;
+	}
+
+	public Date getCreateAt() {
+		return createdAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createdAt = createAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+}
